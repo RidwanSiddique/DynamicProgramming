f9b0a60dcd1bc2f9ca268da428cd29ab69070b08
Ridwan_Siddique <ris152@usask.ca>
2023-12-14 05:20:46 -0600

    Fixed minor unnecessary condition to make code cleaner and smaller.

--- tribonacci/src/Tribonacci.java
+++ tribonacci/src/Tribonacci.java
@@ -10,13 +10,10 @@ public class Tribonacci {
     }
 
     public static int trib(int n, HashMap<Integer, Integer> memo) {
-        if (n == 0 ) {
-            return n;
-        }
-       else if(n == 1){
+        if (n == 0 || n == 1) {
             return 0;
         }
-        else if(n == 2){
+        if(n == 2){
             return 1;
         }
         if (memo.containsKey(n)) {
